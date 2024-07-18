5feeaf686103354a9dd91fa5696c05c298b0a999
AT <athinat690@gmail.com>
2024-07-18 21:31:08 +0100

    fixed searching state bug

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -17,11 +17,11 @@ import { useLocalSearchParams } from "expo-router";
 import { searchAllTables } from "../../api/search/supabase_search";
 import TextMedium18 from "../../components/Typography/TextMedium18";
 import { globalSearch } from "../../constants/filters";
+import TextSemi25 from "../../components/Typography/TextSemi25";
 
 const Search = () => {
   const { query } = useLocalSearchParams();
-  const [returnedData, setReturnedData] = useState({});
-  const [refreshing, setRefreshing] = useState(false);
+  const [returnedData, setReturnedData] = useState([]);
   const [filter, setFilter] = useState("Artist");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -30,10 +30,10 @@ const Search = () => {
       setIsLoading(true);
       try {
         const searchQuery = await searchAllTables(query, filter);
-        setReturnedData(searchQuery);
-        console.log(searchQuery);
+        setReturnedData(searchQuery || []);
       } catch (error) {
         console.error("Error fetching search results:", error.message);
+        setReturnedData([]);
       } finally {
         setIsLoading(false);
       }
@@ -44,20 +44,14 @@ const Search = () => {
     setFilter(filter);
   };
 
-  const onRefresh = async () => {
-    setRefreshing(true);
-    await getSearchResults();
-    setRefreshing(false);
-  };
-
   useEffect(() => {
-    if (query && filter) {
+    if (query) {
       handleSearch();
     }
   }, [query]);
 
   const handleSearch = async () => {
-    if (query && filter) {
+    if (query || filter) {
       const searchData = await getSearchResults();
       if (searchData && Object.keys(searchData).length === 0) {
         await getSearchResults();
@@ -106,10 +100,30 @@ const Search = () => {
               <SearchQuery returnedData={returnedData} filter="NFT" />
             ) : filter === "User" ? (
               <SearchQuery returnedData={returnedData} filter="User" />
+            ) : filter === "Experience" ? (
+              <SearchQuery returnedData={returnedData} filter="Experience" />
             ) : (
-              filter === "Experience" && (
-                <SearchQuery returnedData={returnedData} filter="Experience" />
-              )
+              <></>
+            )}
+
+            {isLoading === false && !query && (
+              <View className="mb-[400px] w-screen items-center">
+                <TextSemi25 extraClasses={"text-left text-purple-300"}>
+                  {" "}
+                  1. Search for something! {"\n"} 2. Choose a category! {"\n"}{" "}
+                  3. Click on Search!
+                </TextSemi25>
+              </View>
+            )}
+
+            {isLoading === false && query && returnedData.length === 0 ? (
+              <View className="mb-[400px] w-screen items-center">
+                <TextSemi25 extraClasses={"text-center"}>
+                  Hmmm, no results. {"\n"} Try something else!
+                </TextSemi25>
+              </View>
+            ) : (
+              <></>
             )}
           </View>
         </BgBlackOverlay>
@@ -119,108 +133,3 @@ const Search = () => {
 };
 
 export default Search;
-
-// import {
-//   View,
-//   Image,
-//   SafeAreaView,
-//   Text,
-//   TouchableOpacity,
-// } from "react-native";
-// import React, { useEffect, useState } from "react";
-// import BgDarkGradient from "../../components/BackgroundGradients/BgDarkGradient";
-// import { images } from "../../constants";
-// import AraxiaHeadBar from "../../components/HeadBars/AraxiaHeadBar";
-// import BgBlackOverlay from "../../components/BackgroundGradients/BgBlackOverlay";
-// import SearchBar from "../../components/Search/SearchBar";
-// import SearchQuery from "../search/[query]";
-// import { useLocalSearchParams } from "expo-router";
-// import { searchAllTables } from "../../api/search/supabase_search";
-// import TextMedium18 from "../../components/Typography/TextMedium18";
-// import { globalSearch } from "../../constants/filters";
-
-// const Search = () => {
-//   const { query } = useLocalSearchParams();
-//   const [returnedData, setReturnedData] = useState({});
-//   const [refreshing, setRefreshing] = useState(false);
-//   const [filter, setFilter] = useState("Artist");
-
-//   const getSearchResults = async () => {
-//     if (query) {
-//       try {
-//         const searchQuery = await searchAllTables(query, filter);
-//         setReturnedData(searchQuery);
-//         console.log(searchQuery);
-//       } catch (error) {
-//         console.error("Error fetching search results:", error.message);
-//       }
-//     }
-//   };
-
-//   const handleFilterPress = (filter) => () => {
-//     setFilter(filter);
-//   };
-
-//   const onRefresh = async () => {
-//     setRefreshing(true);
-//     await getSearchResults();
-//     setRefreshing(false);
-//   };
-
-//   // useEffect(() => {
-
-//   // }, [query, filter]);
-
-//   const handleSearch = async () => {
-//     if (query && filter) {
-//       const searchData = await getSearchResults();
-
-//       if (searchData == []) {
-//         await getSearchResults();
-//       }
-//     }
-//   };
-
-//   return (
-//     <SafeAreaView className="flex-1">
-//       <BgDarkGradient linearGradientMarginTop={"-mt-5"}>
-//         <Image
-//           source={images.loginBG}
-//           resizeMode="contain"
-//           className="w-screen h-full top-0 mt-6 rounded-3xl absolute"
-//         />
-//         <BgBlackOverlay>
-//           <View className="flex-col h-full w-full">
-//             <AraxiaHeadBar />
-//             <View>
-//               <SearchBar
-//                 placeholder={"Search for an experience..."}
-//                 initialQuery={query}
-//                 onSearch={handleSearch}
-//               />
-//             </View>
-//             <View className="flex-row items-center justify-center mt-5">
-//               {globalSearch.map((item) => (
-//                 <TouchableOpacity
-//                   key={item.label}
-//                   className={`px-3 py-2 ${
-//                     filter === item.label ? "bg-purple-500" : "bg-purple-800"
-//                   } rounded-full mr-2`}
-//                   onPress={handleFilterPress(item.label)}
-//                 >
-//                   <TextMedium18>{item.label}</TextMedium18>
-//                 </TouchableOpacity>
-//               ))}
-//             </View>
-//             <SearchQuery
-//               returnedDataNFT={returnedData.globalNFTsResults}
-//               returnedDataArtist={returnedData.userTableResults}
-//             />
-//           </View>
-//         </BgBlackOverlay>
-//       </BgDarkGradient>
-//     </SafeAreaView>
-//   );
-// };
-
-// export default Search;
